a1ca3673041b43d58a56a29bd6d2ece89715ac3e
mohanadc <mohanad.mohamed@ucdconnect.ie>
2026-02-19 16:24:58 +0000

    Initial Commit

---
+++ FYP_22311136/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FYP_22311136
+//
+//  Created by Mohanad Magdi Mohamed on 19/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FYP_22311136/FYP_22311136App.swift
@@ -0,0 +1,17 @@
+//
+//  FYP_22311136App.swift
+//  FYP_22311136
+//
+//  Created by Mohanad Magdi Mohamed on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FYP_22311136App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
